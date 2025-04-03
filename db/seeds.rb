ae9a42475433c97d74ad30a3b7af1323f5e9436f
Zece <zece.dev@proton.me>
2025-04-03 11:25:58 -0300

    Add seeds with cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Car.create(brand: 'Toyota', model: 'Corolla', year: 2025)
+Car.create(brand: 'Toyota', model: 'Corolla', year: 2024)
+
+Car.create(brand: 'Ford', model: 'Fiesta', year: 2014)
+Car.create(brand: 'Ford', model: 'Mustang', year: 2020)
+
+Car.create(brand: 'Peugeot', model: '208', year: 2004)
+Car.create(brand: 'Peugeot', model: 'Partner', year: 2021)
