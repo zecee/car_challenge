2b9f62b67c46f225c82a77c1409e5ee93275f211
Zece <zece.dev@proton.me>
2025-04-03 11:24:46 -0300

    Protect private endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
-class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+class ApplicationController < ActionController::API
+  before_action :authenticate_user
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate_user
+    header = request.headers["Authorization"]
+    token = header.split(" ").last if header.present?
+
+    decoded = JsonWebToken.decode(token)
+    @current_user = User.find_by(id: decoded[:user_id]) if decoded
+
+    render json: { error: "No autorizado" }, status: :unauthorized unless @current_user
+  end
 end
